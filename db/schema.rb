dbe377a6f4bc640ef486d4beb2e30d7d4d0c3c15
TakammotoYurie <happyyuupy@gmail.com>
2022-01-10 03:45:25 +0900

    add: migrationの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_165434) do
+ActiveRecord::Schema.define(version: 2022_01_09_091948) do
 
   create_table "users", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.string "nameimage_url"
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "name", null: false
+    t.string "image_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
 end
