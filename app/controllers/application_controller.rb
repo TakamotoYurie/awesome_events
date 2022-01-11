626422a06cc4c6d19817a9e335ae9903af920f7b
TakammotoYurie <happyyuupy@gmail.com>
2022-01-11 17:34:38 +0900

    add: ログイン状態の管理設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   def current_user
     return unless session[:user_id]
     # sessionにユーザーidが入っていなければ下のコードは実行しない
-    @current_user || = User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id])
   end
 
   def authenticate
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,12 @@
 class EventsController < ApplicationController
   def new
     @event = current_user.created_events.build
+  end
 
-    if event.save
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
       redirect_to @event, notice: "作成しました"
     end
   end
